117e3d4da2d4ca24d49329ce8b5bbe9ec30c5c1a
alirezajs <alireza.varmaghani@gmail.com>
2021-10-03 19:09:31 +0330

    create header menu

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeaderComponent } from './components/header/header.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
